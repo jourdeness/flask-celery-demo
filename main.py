2ff9609b7731ab6857456c604e0109242780c4a9
coreyteng <oh8048@gmail.com>
2022-03-18 17:22:42 +0800

    .

--- main.py
+++ main.py
@@ -10,21 +10,21 @@ app = create_app('default')
 def index():
     return "Hello World"
 
+
 @app.route('/error')
 def error_m():
     app.logger.info('info log')
     try:
         raise
     except Exception as e:
-        app.logger.warning(e,exc_info=True)
+        app.logger.warning(e, exc_info=True)
     return "Write error log"
 
 
-@app.route('/get_route_map',methods=['GET'])
+@app.route('/get_route_map', methods=['GET'])
 def getroutemap():
     return str(app.url_map)
 
 
-
 if __name__ == '__main__':
-    app.run(app.config['HOST'],app.config['PORT'],app.config['DEBUG'])
+    app.run(app.config['HOST'], app.config['PORT'], app.config['DEBUG'])
--- tasks/add.py
+++ tasks/add.py
@@ -1,5 +1,6 @@
 from app import celery
 
+
 @celery.task(name='add')
 def add(x, y):
     print('Hello job add')
